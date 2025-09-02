63bd059821639608f33203e0d0a1424ac16d04f8
LaurenceManuelSalaber-UCM <laurencemanuelsalaber@gmail.com>
2025-09-02 19:33:40 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+/**
+ * This program demonstrates the three types of comments in Java:
+ * 1. Single-line comment
+ * 2. Multi-line comment
+ * 3. Documentation comment
+ */
+public class Comments {
+    public static void main(String[] args) {
+        // Displaying a welcome message (single-line comment)
+        System.out.println("Welcome to the Comments demo!");
+
+        /*
+          The next line prints another message.
+          This is a multi-line comment.
+          You can use it for longer explanations.
+        */
+        System.out.println("This program uses all three types of comments.");
+    }
+}

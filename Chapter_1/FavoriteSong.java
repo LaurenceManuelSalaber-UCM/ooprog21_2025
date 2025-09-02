c8b2dbf334f8843beca1ed943e5382325bfb10ba
LaurenceManuelSalaber-UCM <laurencemanuelsalaber@gmail.com>
2025-09-02 19:22:04 +0800

    Delete Chapter_1/FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++
@@ -1,8 +0,0 @@
-public class FavoriteSong {
-    public static void main(String[] args) {
-        System.out.println("Summer nights all align");
-        System.out.println("As we drown in the moonlight");
-        System.out.println("We collide in plain sight");
-        System.out.println("Yeah, I know we'll be alright");
-    }
-}

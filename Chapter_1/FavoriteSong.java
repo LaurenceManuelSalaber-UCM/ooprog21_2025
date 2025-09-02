f71315e44fcc3dab89257989f7b11f590cacab35
LaurenceManuelSalaber-UCM <laurencemanuelsalaber@gmail.com>
2025-09-02 19:11:20 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Summer nights all align");
+        System.out.println("As we drown in the moonlight");
+        System.out.println("We collide in plain sight");
+        System.out.println("Yeah, I know we'll be alright");
+    }
+}

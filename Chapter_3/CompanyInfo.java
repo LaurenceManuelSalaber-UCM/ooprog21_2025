bfaebe4b560a05967f01f025196ee2e6cc09a960
LaurenceManuelSalaber-UCM <laurencemanuelsalaber@gmail.com>
2025-09-15 09:03:30 +0800

    Add files via upload

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,14 @@
+public class CompanyInfo {
+    public static void main(String[] args) {
+    
+        System.out.println("Smart Electronics");
+        displayHours();
+    }
+
+    public static void displayHours() {
+    
+        System.out.println("Monday - Friday 8 am to 10 pm");
+        System.out.println("Saturday        8 am to 4pm");
+        System.out.println("Sunday          Closed");
+    }
+}
